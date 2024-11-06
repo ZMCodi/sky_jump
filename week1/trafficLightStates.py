ff15e3f2ef1a51e2da5b9b7edae32ed056f08a91
ZMCodi Ubuntu <zaim.mohsin@gmail.com>
2024-11-06 22:47:12 +0000

    done crossword

--- week1/trafficLightStates.py
+++
@@ -1,9 +0,0 @@
-def printState():
-    print("Red Light is " + str(redLight))
-    print("Yellow Light is " + str(yellowLight))
-    print("Green Light is " + str(greenLight))
-
-redLight = 1
-yellowLight = 0
-greenLight = 0
-printState()
